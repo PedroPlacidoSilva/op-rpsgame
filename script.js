f644cd0f47f781a828088325f38f024e76171da7
Pedro Silva <ppspedro@gmail.com>
2022-09-07 19:29:41 +0100

    Added store input of the user for the game and stored in variable

---
+++ script.js
@@ -0,0 +1,34 @@
+// PROGRAMGame Rock/Paper/Scissors
+// Ask the user to imput Rock Paper Scissors
+    function userInfo() {
+        let userChoice = document.getElementById("userInput").value;
+        console.log(userChoice)
+        alert(userChoice)
+        return userChoice;
+    }
+    
+    let userChoice = userInfo();
+   
+
+//Store the information on a variable
+
+
+
+// IF user imput valid
+    // Obtain a random Rock Paper Scissors by the computer
+    // Store the information on a variable
+    // Show the result of the random data of the computer
+    
+    // Test user vs imput who is the winner (PC or user)
+    
+        // IF User is the winner
+            // Show message to the user that he is the winner
+            // Show aditional message. Choice of the user beats Choice of the PC
+        // ELSE
+            // Show message that the user is not the winner
+            // Show aditional message. Choice of the user does not beat Choice of the PC
+//  ELSE
+        // Ask user to enter information again
+
+
+       
